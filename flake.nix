20a1c1ccb532986cec5952fd505d1fd4738d32ce
niten <niten@fudo.org>
2023-05-16 15:46:50 -0700

    update deps with the new deps manager

--- flake.nix
+++ flake.nix
@@ -2,24 +2,35 @@
   description = "Tattler Notification Listener";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-22.05";
+    nixpkgs.url = "nixpkgs/nixos-22.11";
     utils.url = "github:numtide/flake-utils";
     helpers = {
-      url = "git+https://git.fudo.org/fudo-public/nix-helpers.git";
+      url =
+        "git+https://git.fudo.org/fudo-public/nix-helpers.git?ref=with-deps";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    notifierClj = {
+      url = "git+https://git.fudo.org/fudo-public/notifier.git";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, utils, helpers, ... }:
+  outputs = { self, nixpkgs, utils, helpers, notifierClj, ... }:
     utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages."${system}";
+      let
+        inherit (helpers.packages."${system}") mkClojureBin;
+        pkgs = nixpkgs.legacyPackages."${system}";
+        cljLibs = {
+          "org.fudo/notifier" = "${notifierClj.packages."${system}".notifier}";
+        };
       in {
         packages = rec {
           default = tattler;
-          tattler = helpers.packages."${system}".mkClojureBin {
+          tattler = mkClojureBin {
             name = "org.fudo/tattler";
             primaryNamespace = "tattler.cli";
             src = ./.;
+            inherit cljLibs;
           };
         };
 
@@ -27,7 +38,7 @@
           default = updateDeps;
           updateDeps = pkgs.mkShell {
             buildInputs = with helpers.packages."${system}";
-              [ updateClojureDeps ];
+              [ (updateCljDeps cljLibs) ];
           };
           tattler = pkgs.mkShell {
             buildInputs = with self.packages."${system}"; [ tattler-server ];
